75fc1536102e3f5ec26fc589571063fda8e45c64
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-13 00:02:42 +0100

    removed steel and added titanium to LDS

--- prototypes/vanilla-recipes.lua
+++ prototypes/vanilla-recipes.lua
@@ -1,2 +1,5 @@
 table.insert(data.raw["recipe"]["construction-robot"].ingredients, PM.ingredient("dd-titanium-plate", 2))
 table.insert(data.raw["recipe"]["logistic-robot"].ingredients, PM.ingredient("dd-titanium-plate", 2))
+
+table.remove(data.raw["recipe"]["low-density-structure"].ingredients, 1)
+table.insert(data.raw["recipe"]["low-density-structure"].ingredients, PM.ingredient("dd-titanium-plate", 4))
