9027fe24f4b8c12ea6497b75cdf4efa9f717e504
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-12 23:10:44 +0100

    stole ore graphics from PM, require()'d item file

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -4,14 +4,21 @@ data:extend({
         type = "item",
         name = "dd-titanium-ore",
         icon_size = 64,
-        icon = "__double-dip__/graphics/icons/placeholder.png",
+        icon = "__double-dip__/graphics/icons/titanium-ore.png",
+        pictures =
+        {
+            {filename = "__periodic-madness__/graphics/icons/ores/titanium-ore.png", size = 64, scale = 0.5},
+            {filename = "__periodic-madness__/graphics/icons/ores/titanium-ore-2.png", size = 64, scale = 0.5},
+            {filename = "__periodic-madness__/graphics/icons/ores/titanium-ore-3.png", size = 64, scale = 0.5},
+            {filename = "__periodic-madness__/graphics/icons/ores/titanium-ore-4.png", size = 64, scale = 0.5},
+        },
         stack_size = 50
     },
     {
         type = "item",
         name = "dd-titanium-plate",
         icon_size = 64,
-        icon = "__double-dip__/graphics/icons/placeholder.png",
+        icon = "__double-dip__/graphics/icons/titanium-ore.png",
         stack_size = 100
     }
 })
