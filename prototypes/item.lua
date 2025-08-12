30fd327645d87991bb394bfe64e65abeb92b4e2d
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-12 22:52:15 +0100

    inital commit

---
+++ prototypes/item.lua
@@ -0,0 +1,17 @@
+data:extend({
+
+    {
+        type = "item",
+        name = "dd-titanium-ore",
+        icon_size = 64,
+        icon = "__double-dip__/graphics/icons/placeholder.png",
+        stack_size = 50
+    },
+    {
+        type = "item",
+        name = "dd-titanium-plate",
+        icon_size = 64,
+        icon = "__double-dip__/graphics/icons/placeholder.png",
+        stack_size = 50
+    }
+})
