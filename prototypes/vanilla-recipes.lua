a53f427437d1e041603ebb041174bc9a153e8fe0
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-13 10:06:03 +0100

    added EEU's to assembling machine 3 recipe, lowered speed module amount

--- prototypes/vanilla-recipes.lua
+++ prototypes/vanilla-recipes.lua
@@ -8,3 +8,10 @@ table.remove(data.raw["recipe"]["uranium-fuel-cell"].ingredients, 1)
 table.insert(data.raw["recipe"]["uranium-fuel-cell"].ingredients, PM.ingredient("dd-titanium-plate", 10))
 
 table.insert(data.raw["recipe"]["nuclear-fuel-reprocessing"].results, PM.product_chance("dd-titanium-plate", 2, 0.33))
+
+data.raw["recipe"]["assembling-machine"].ingredients = 
+{
+    PM.ingredient("assembling-machine-2", 2),
+    PM.ingredient("speed-module", 2),
+    PM.ingredient("electric-engine-unit", 1)
+}
